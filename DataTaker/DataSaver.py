b8f35f74576c5136f2d01ec9f92dc8f10030ea7c
zackorenberg <zachary.berkson-korenberg@mail.mcgill.ca>
2019-11-03 12:18:22 -0500

    Final upload
    Final upload

---
+++ DataTaker/DataSaver.py
@@ -0,0 +1,43 @@
+
+import os
+import imageio
+import time
+import LocalVars
+
+
+
+class DataSaver(object):
+    def __init__(self):
+        self.path = None
+        pass
+
+
+    def createSavePath(self,path):
+        self.path = os.path.join(path,str(time.time())) + os.sep
+        os.mkdir(self.path)
+        return self.path
+
+    def createGif(self):
+        if not LocalVars.SAVE_GIF:
+            return
+        if self.path is None:
+            return
+        images = [ ]
+        with imageio.get_writer(os.path.join(self.path,"simulation.gif"), mode='I') as writer:
+            for f in os.listdir(self.path):
+                if ".png" in f:
+                    image = imageio.imread(f)
+                    writer.append_data(image)
+
+    def saveData(self,data):
+        lines = []
+        for dat in data:
+            print(dat)
+            lines.append( ",".join([str(dat[0]),str(dat[1])]) )
+        print(lines)
+        with open(os.path.join(self.path,"temps.dat"), "w") as f:
+            f.writelines(lines)
+
+
+
+
---
+++ LocalVars.py
@@ -0,0 +1,24 @@
+
+SAVE_GIF = False # only enable true if you know imageio will work
+CYCLE_VIRTUAL = False
+
+CMax = 25
+CMin = 0
+
+DensityRange = [1,50]
+ViscosityRange = [1, 20]
+MultiplierRange = [20, 750]
+
+DensityDefault = 10
+ViscosityDefault = 10
+MultiplierDefault = 50
+
+
+PaneWidth = 2.6 #CM
+PaneHeight = 260 #CM
+
+PlotTitle = "Temperature Contour"
+PlotXLabel = "x (cm)"
+PlotYLabel = "y (cm)"
+PlotCMLabel = "Temperature (Celsius)"
+PlotWindowTitle = 'Double-Pane Window Temperature Simulator'
